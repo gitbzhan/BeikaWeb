440eec13ae738ad303c9033601ce4115a29900e5
Beika zhan <beika.zhan@mckesson.ca>
2017-10-29 11:39:32 -0400

    adding the first file.

--- BeikaWeb/Startup.cs
+++ BeikaWeb/Startup.cs
@@ -27,7 +27,7 @@ namespace BeikaWeb
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!test");
             });
         }
     }
